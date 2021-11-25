11e508ed809a80ab5486d5eb63296632efa6bb5f
Gowthamap <gowthamap55555@gmail.com>
2021-11-25 16:51:26 +0530

    debuggin in browser

---
+++ debug/quote.js
@@ -0,0 +1,11 @@
+console.log('It\'s so exciting!');
+
+console.log("It's so exciting!");
+
+console.log("They said, \"you've won!\" ");
+
+var message = 'Hey, she\'s the winner!';
+console.log(message);
+
+var response = "She said, \"I can\'t believe I won.\" "
+console.log(response);
